a6150edcfde27b2172640c4677bb8bf725aaa10d
Dipongkor-Roy <juniordipu@gmail.com>
2023-08-08 14:24:30 +0600

    fullserver setup done

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const express = require("express");
 const app = express();
-const cors=require('cors')
-const port =9000;
-
+const cors = require("cors");
+const port = 9000;
+const news = require("./Data/news.json");
 app.use(cors());
 
-const categories=require('./Data/Categories.json')
-app.get('/', (req, res) => {
+const categories = require("./Data/Categories.json");
+app.get("/", (req, res) => {
   res.send("News Api Running");
 });
 
-app.get('/news-categories',(req,res)=>{
-    res.send(categories)
+app.get("/news-categories", (req, res) => {
+  res.send(categories);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "8") {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+app.get('/news',(req,res)=>{
+  res.send(news);
 })
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
+});
 app.listen(port, () => {
   console.log("open news running in port", port);
 });
